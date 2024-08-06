43d36345eb593f1c760ad3f33bf83b1e1c66940a
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-08-06 02:39:13 -0400

    .

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,5 @@
+import { lusitana } from "@/app/ui/fonts";
+
 export default function Customers() {
-  return <p>Customers Page</p>;
+  return <h1 className={`${lusitana.className} text-2xl`}>Customer Page</h1>;
 }
