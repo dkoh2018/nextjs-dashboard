e246c4c91734893b74be3bc777acd3fa3db542ab
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-08-06 02:42:52 -0400

    m

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,12 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
+            sup, this is my practice board. this shows the complete guide to Next.js dashboard
+            course
+          </p>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
