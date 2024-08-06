614a554358e813d68192a54dab0951109fb148d5
dkoh2018 <105094753+dkoh2018@users.noreply.github.com>
2024-08-06 01:46:35 -0400

    query string in invoices

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 import NavLinks from "../ui/dashboard/nav-links";
 
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
